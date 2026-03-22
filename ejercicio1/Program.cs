883221c757dc371e64290b215c676655322327ce
Milagro Tejada <tj0156032025@unab.edu.sv>
2026-03-21 22:56:16 -0600

    Agregando .gitignore

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Producto simple
+public class Producto
+{
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+
+    public Producto(string nombre, double precio)
+    {
+        Nombre = nombre;
+        Precio = precio;
+    }
+}
+
+// Clase principal que se va a construir
+public class Pedido
+{
+    public string Cliente { get; set; }
+    public List<Producto> Productos { get; set; } = new List<Producto>();
+    public string DireccionEnvio { get; set; }
+    public string MetodoPago { get; set; }
+    public double Descuento { get; set; }
+    public double Impuestos { get; set; }
+
+    public void MostrarPedido()
+    {
+        Console.WriteLine("Cliente: " + Cliente);
+        Console.WriteLine("Productos:");
+        foreach (var p in Productos)
+        {
+            Console.WriteLine($"- {p.Nombre}: ${p.Precio}");
+        }
+        Console.WriteLine("Dirección: " + DireccionEnvio);
+        Console.WriteLine("Método de Pago: " + MetodoPago);
+        Console.WriteLine("Descuento: " + Descuento);
+        Console.WriteLine("Impuestos: " + Impuestos);
+    }
+}
+
+// Builder
+public class PedidoBuilder
+{
+    private Pedido pedido = new Pedido();
+
+    public PedidoBuilder SetCliente(string cliente)
+    {
+        pedido.Cliente = cliente;
+        return this;
+    }
+
+    public PedidoBuilder AgregarProducto(Producto producto)
+    {
+        pedido.Productos.Add(producto);
+        return this;
+    }
+
+    public PedidoBuilder SetDireccion(string direccion)
+    {
+        pedido.DireccionEnvio = direccion;
+        return this;
+    }
+
+    public PedidoBuilder SetMetodoPago(string metodo)
+    {
+        pedido.MetodoPago = metodo;
+        return this;
+    }
+
+    public PedidoBuilder SetDescuento(double descuento)
+    {
+        pedido.Descuento = descuento;
+        return this;
+    }
+
+    public PedidoBuilder SetImpuestos(double impuestos)
+    {
+        pedido.Impuestos = impuestos;
+        return this;
+    }
+
+    public Pedido Build()
+    {
+        return pedido;
+    }
+}
+
+// Programa principal
+class Program
+{
+    static void Main()
+    {
+        var pedido = new PedidoBuilder()
+            .SetCliente("Juan Pérez")
+            .AgregarProducto(new Producto("Laptop", 800))
+            .AgregarProducto(new Producto("Mouse", 25))
+            .SetDireccion("San Salvador")
+            .SetMetodoPago("Tarjeta")
+            .SetDescuento(50)
+            .SetImpuestos(13)
+            .Build();
+
+        pedido.MostrarPedido();
+    }
+}
---
+++ ejercicio2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+// Clase que representa la configuración
+public class ConfiguracionBD
+{
+    public string Servidor { get; set; }
+    public string BaseDatos { get; set; }
+    public string Usuario { get; set; }
+    public string Password { get; set; }
+    public int Puerto { get; set; }
+    public int Timeout { get; set; }
+
+    public void MostrarConfig()
+    {
+        Console.WriteLine("Servidor: " + Servidor);
+        Console.WriteLine("Base de Datos: " + BaseDatos);
+        Console.WriteLine("Usuario: " + Usuario);
+        Console.WriteLine("Password: " + Password);
+        Console.WriteLine("Puerto: " + Puerto);
+        Console.WriteLine("Timeout: " + Timeout);
+    }
+}
+
+// Builder
+public class ConfiguracionBuilder
+{
+    private ConfiguracionBD config = new ConfiguracionBD();
+
+    public ConfiguracionBuilder SetServidor(string servidor)
+    {
+        config.Servidor = servidor;
+        return this;
+    }
+
+    public ConfiguracionBuilder SetBaseDatos(string bd)
+    {
+        config.BaseDatos = bd;
+        return this;
+    }
+
+    public ConfiguracionBuilder SetUsuario(string usuario)
+    {
+        config.Usuario = usuario;
+        return this;
+    }
+
+    public ConfiguracionBuilder SetPassword(string password)
+    {
+        config.Password = password;
+        return this;
+    }
+
+    public ConfiguracionBuilder SetPuerto(int puerto)
+    {
+        config.Puerto = puerto;
+        return this;
+    }
+
+    public ConfiguracionBuilder SetTimeout(int timeout)
+    {
+        config.Timeout = timeout;
+        return this;
+    }
+
+    public ConfiguracionBD Build()
+    {
+        return config;
+    }
+}
+
+// Programa principal
+class Program
+{
+    static void Main()
+    {
+        var config = new ConfiguracionBuilder()
+            .SetServidor("localhost")
+            .SetBaseDatos("EmpresaDB")
+            .SetUsuario("admin")
+            .SetPassword("1234")
+            .SetPuerto(1433)
+            .SetTimeout(30)
+            .Build();
+
+        config.MostrarConfig();
+    }
+}
